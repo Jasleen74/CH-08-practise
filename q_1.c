02c9979000f9be18b7b871289557a7da3a6c8c47
Jasleen74 <preetjee@gmail.com>
2025-10-25 19:41:44 +0530

    Write a program to take string as an input from the user using %c and %s confirm that the strings are equal

---
+++ q_1.c
@@ -0,0 +1,31 @@
+/*Write a program to take string as an input from the user using %c and %s confirm that the strings are equal.*/
+#include<stdio.h>
+int main() {
+
+    char name[50];
+
+    //Using %s 
+    // printf("Enter ur name pls : ");
+    //scanf("%s",name);
+    
+    //printf("Ur name is : %s\n", name);
+
+
+    // Using %c
+    char myname[8];
+    printf("enter ur name pls : ");
+
+    for (int i = 0 ;i <7; i ++){
+        scanf("%c",&myname[i]);
+        fflush(stdin);          //we need to add this cuz when we press enter it reads it as a character too
+    }
+    myname[7]='\0';
+
+    printf("%s",myname);
+
+
+//
+     
+  
+ return 0;
+}
