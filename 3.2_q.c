e269bebc3103fdf48b8a0bb5ae87fb994137104d
Jasleen74 <preetjee@gmail.com>
2025-10-26 12:04:47 +0530

    slicing from m as start and n as end

--- 3.2_q.c
+++ 3.2_q.c
@@ -1,6 +1,4 @@
-/*4. Write a function slice() to slice a string. It should change the original string such
-that it is now the sliced string. Take ‘m’ and ‘n’ as the start and ending position
-for slice.*/
+/**/
 #include<stdio.h>
 char * slice(char string[], int m , int n ){
     int i = 0 ;
